178ba788a570334dfb3f446be53a6f413fd9fcbe
Rich Alcabes <alcabes.richard@gmail.com>
2023-06-21 16:58:54 -0500

    add README.md

---
+++ 1-database-sql/sql_intro.sql
@@ -0,0 +1,12 @@
+SHOW TABLES;
+SHOW DATABASES;
+USE chipotle;
+SHOW TABLES;
+SELECT *
+FROM mysql.user;
+SELECT user, host
+FROM mysql.user;
+SELECT * 
+FROM mysql.help_topic;
+SELECT help_topic_id, help_category_id, url
+FROM mysql.help_topic;
---
+++ README.md
@@ -0,0 +1,2 @@
+## This repo shall serve as a storage repo for a review of all things CodeUp.
+# 1-DATABASE-SQL
